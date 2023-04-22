c0d56c624106dda8695fd829e4b1ac2cadcde91e
andyaspel <aspelandy@outlook.com>
2023-04-23 01:03:42 +0200

    refactoring + minor changes

--- work/index.js
+++ work/index.js
@@ -1,5 +1,5 @@
-import {toDeg, toRad,average} from "./js/general.js";
-import {drawPoint,drawLine, drawText, drawCoordinateSystem} from "./js/draw.js";
+import { toDeg, toRad, average } from "./js/general.js";
+import { drawPoint, drawLine, drawText, drawCoordinateSystem } from "./js/draw.js";
 const myCanvas = document.getElementById("myCanvas");
 const chartCanvas = document.getElementById("chartCanvas");
 
@@ -21,12 +21,11 @@ const c = 100;
 
 const A = { x: 0, y: 0 };
 const B = {
-    x: Math.cos(-theta) * c,
-    y: Math.sin(-theta) * c
+    x: Math.cos(theta) * c,
+    y: Math.sin(theta) * c
 };
-const C = { x: 0, y: 0 };
-console.log(theta);
-console.log(ctx);
+const C = { x: B, y: 0 };
+
 ctx.translate(offset.x, offset.y);
 chartCtx.translate(chartOffset.x, chartOffset.y);
 
@@ -34,7 +33,7 @@ drawCoordinateSystem(chartCtx, chartOffset);
 
 update(ctx);
 document.onwheel = (event) => {
-    theta -= toRad(Math.sign(event.deltaY));
+    theta += toRad(Math.sign(event.deltaY));
 
     B.x = Math.cos(theta) * c;
     B.y = Math.sin(theta) * c;
@@ -52,7 +51,7 @@ function update(ctx) {
     const tan = Math.tan(theta);
 
     const T = {
-        x: Math.sign(cos) * Math.hypot(1, tan) * c,
+        x: Math.sign(cos) * Math.sqrt(1 * 1 + tan * tan) * c,
         y: 0
     };
 
@@ -62,58 +61,75 @@ function update(ctx) {
     drawCoordinateSystem(ctx, offset);
 
     drawText(
-        "sin = " + sin.toFixed(4),
+        "sinθ = " + sin.toFixed(4),
         { x: -offset.x / 2, y: offset.y * 0.7 },
-        "red",ctx
+        "red", ctx
     );
 
     drawText(
-        "cos = " + cos.toFixed(4),
+        "cosθ = " + cos.toFixed(4),
         { x: -offset.x / 2, y: offset.y * 0.8 },
-        "blue",ctx
+        "blue", ctx
     );
 
     drawText(
-        "tan = " + tan.toFixed(4),
+        "tanθ = " + tan.toFixed(4),
         { x: -offset.x / 2, y: offset.y * 0.9 },
-        "magenta",ctx
+        "magenta", ctx
     );
 
     drawText(
         "θ = " + theta.toFixed(4) + " (" +
         toDeg(theta).toFixed(3).
             toString().padStart(2, " ") + "°)",
-        { x: offset.x / 2, y: offset.y * 0.7 },"black",ctx
+        { x: offset.x / 2, y: offset.y * 0.6 }, "black", ctx
+    );
+    drawText(
+        "c = " + c.toFixed(4),
+        { x: offset.x / 2, y: offset.y * 0.7 },
+        "green", ctx
+    );
+    drawText(
+        "a = " + B.x.toFixed(4),
+        { x: offset.x / 2, y: offset.y * 0.8 },
+        "green", ctx
     );
 
-    drawLine(A, B,"black", ctx);
-    drawText("1", average(A, B),"black",ctx);
-    drawLine(A, C, "blue",ctx);
-    drawText("cos", average(A, C), "blue",ctx);
-    drawLine(B, C, "red",ctx);
-    drawText("sin", average(B, C), "red",ctx);
-    drawLine(B, T, "magenta",ctx);
-    drawText("tan", average(B, T), "magenta",ctx);
+    drawText(
+        "b = " + B.y.toFixed(4),
+        { x: offset.x / 2, y: offset.y * 0.9 },
+        "green", ctx
+    );
 
-    drawText("θ", A,"black",ctx);
+
+    drawLine(A, B, "black", ctx);
+    drawText("h", average(A, B), "black", ctx);
+    drawLine(A, C, "blue", ctx);
+    drawText("a", average(A, C), "blue", ctx);
+    drawLine(B, C, "red", ctx);
+    drawText("o", average(B, C), "red", ctx);
+    drawLine(B, T, "magenta", ctx);
+    drawText("t", average(B, T), "magenta", ctx);
+
+    // drawText("0,0", A, "black", ctx);
 
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-    ctx.arc(0, 0, c, 0, theta, theta < 0);
+    ctx.lineWidth = 1;
+    ctx.arc(0, 0, c, 0, theta);
     ctx.stroke();
 
-    const chartScaler = chartOffset.y * 0.5;
+    const chartScaler = chartOffset.y * 0.2;
     drawPoint({
         x: theta * chartScaler,
         y: sin * chartScaler
-    }, 2, "red",chartCtx);
+    }, 1, "red", chartCtx);
     drawPoint({
         x: theta * chartScaler,
         y: cos * chartScaler
-    }, 2, "blue",chartCtx);
+    }, 1, "blue", chartCtx);
     drawPoint({
         x: theta * chartScaler,
         y: tan * chartScaler
-    }, 2, "magenta",chartCtx);
+    }, 1, "magenta", chartCtx);
 }
--- work/js/draw.js
+++ work/js/draw.js
@@ -1,16 +1,16 @@
 //  canvas draw functions
 // example drawPoint({x:1,y:-2}, 2,"magenta")
-export function drawPoint(loc, size = 2, color = "black",chartCtx) {
+export function drawPoint(loc, size = 1, color = "black",chartCtx) {
     chartCtx.beginPath();
     chartCtx.fillStyle = color;
-    chartCtx.arc(loc.x, loc.y, size / 2, 0, Math.PI * 2);
+    chartCtx.arc(loc.x, loc.y, size / 2, 0, Math.PI*2 );
     chartCtx.fill();
 }
 // example drawLine({x:0,y:0}, {x:1, y"-1}, "blue");
 export function drawLine(p1, p2, color = "black",ctx) {
     ctx.beginPath();
     ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 1;
     ctx.moveTo(p1.x, p1.y);
     ctx.lineTo(p2.x, p2.y);
     ctx.stroke();
@@ -20,10 +20,10 @@ export function drawText(text, loc, color = "black",ctx) {
     ctx.beginPath();
     ctx.fillStyle = color;
     ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.font = "bold 18px Courier";
+    ctx.textBaseline = "bottom";
+    ctx.font = "bold 16px Courier";
     ctx.strokeStyle = "white";
-    ctx.lineWidth = 7;
+    ctx.lineWidth = 1;
     ctx.strokeText(text, loc.x, loc.y);
     ctx.fillText(text, loc.x, loc.y);
 }
@@ -35,7 +35,7 @@ export function drawCoordinateSystem(ctx, offset) {
     ctx.lineTo(-offset.x, 0);
     ctx.moveTo(0, offset.y);
     ctx.lineTo(0, -offset.y);
-    ctx.setLineDash([4, 2]);
+    ctx.setLineDash([4, 3]);
     ctx.lineWidth = 1;
     ctx.strokeStyle = "gray";
     ctx.stroke();
